c79df1ec1a64aa56d55b097b28b2026cb02976b9
BunnyK <52962743+KtheBunny@users.noreply.github.com>
2025-11-17 20:38:12 +0800

    使用forwardRef修正setRef setFloating問題

--- src/components/SkillTreeCanvasNode.jsx
+++ src/components/SkillTreeCanvasNode.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
-import SkillTreeCanvasNodeDetail from "./SkillTreeCanvasNodeDetail";
 import { motion } from "motion/react";
+import { useFloating, offset, flip, shift } from "@floating-ui/react";
+
+import SkillTreeCanvasNodeDetail from "./SkillTreeCanvasNodeDetail";
 
 export default function SkillTreeCanvasNode({
   id,
@@ -14,14 +16,20 @@ export default function SkillTreeCanvasNode({
   workLink,
   mastery,
   masteryDescriptions,
-  prerequisites
+  prerequisites,
 }) {
   const [open, setOpen] = useState(false);
+
+  const { refs, floatingStyles } = useFloating({
+    placement: "top",
+    middleware: [offset(0), flip(), shift()],
+  });
 
   return (
     <>
       {/* 技能節點本體 */}
       <div
+        ref={refs.setReference}
         className="absolute -translate-x-1/2 -translate-y-1/2"
         style={{ top: y, left: x }}
         onMouseEnter={() => setOpen(true)}
@@ -32,12 +40,14 @@ export default function SkillTreeCanvasNode({
           whileHover={{ scale: 1.1 }}
           transition={{ type: "spring", stiffness: 300, damping: 20 }}
         >
-          <div className="
+          <div
+            className="
             w-12 h-12 rounded-full
             bg-[rgba(0,0,0,0.25)]
             border-2 border-[rgba(255,255,255,0.5)]
             flex items-center justify-center
-          ">
+          "
+          >
             <span className="material-symbols-outlined">{icon}</span>
           </div>
         </motion.div>
@@ -46,8 +56,8 @@ export default function SkillTreeCanvasNode({
       {/* Node Detail（使用 Portal） */}
       <SkillTreeCanvasNodeDetail
         open={open}
-        x={x}
-        y={y}
+        ref={refs.setFloating}
+        style={floatingStyles}
         icon={icon}
         title={title}
         type={type}

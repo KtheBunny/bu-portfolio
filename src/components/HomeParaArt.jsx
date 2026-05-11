1999b7eb027826e89b93a9ae9854506e592fec04
BunnyK <keithyuen0501@gmail.com>
2026-05-12 01:10:40 +0800

    性能修正: 把gif換成apng 減少parallax層數

--- src/components/HomeParaArt.jsx
+++ src/components/HomeParaArt.jsx
@@ -1,9 +1,9 @@
 import { MouseParallax } from "react-just-parallax";
 
 import z1 from "../assets/art/home-illustration-z101.webp";
-import z2 from "../assets/art/home-illustration-z102.webp";
+
 import z3 from "../assets/art/home-illustration-z103.webp";
-import z4 from "../assets/art/home-illustration-z104.webp";
+
 import z5 from "../assets/art/home-illustration-z105.webp";
 import z6 from "../assets/art/home-illustration-z106.webp";
 import z7 from "../assets/art/home-illustration-z107.webp";
@@ -15,127 +15,101 @@ import pixiv from "../assets/logo/pixiv.svg";
 export default function HomeParaArt() {
   return (
     <>
-    <div className="relative h-full overflow-hidden [contain:layout_paint]">
-      <div
-        className="absolute bottom-10 right-10 z-10 cursor-pointer"
-        style={{ filter: "brightness(0) invert(1)" }}
-        onClick={() =>
-          window
-            .open("https://www.pixiv.net/artworks/137194516", "_blank")
-            .focus()
-        }
-      >
-        <img src={pixiv} className="mask h-6 w-6" />
-      </div>
+      <div className="relative h-full overflow-hidden [contain:layout_paint]">
+        <div
+          className="absolute bottom-10 right-10 z-10 cursor-pointer"
+          style={{ filter: "brightness(0) invert(1)" }}
+          onClick={() =>
+            window
+              .open("https://www.pixiv.net/artworks/137194516", "_blank")
+              .focus()
+          }
+        >
+          <img src={pixiv} className="mask h-6 w-6" />
+        </div>
 
-      <img
-        className="absolute h-full w-full object-cover object-center"
-        src={z1}
-      />
-
-      <MouseParallax
-        isAbsolutelyPositioned
-        strength={0.01}
-        zIndex={2}
-        shouldPause={false}
-        enableOnTouchDevice={true}
-      >
         <img
-          className="absolute h-full w-full scale-[101%] object-cover object-center"
-          src={z2}
+          className="absolute h-full w-full object-cover object-center"
+          src={z1}
         />
-      </MouseParallax>
 
-      <MouseParallax
-        isAbsolutelyPositioned
-        strength={0.03}
-        zIndex={3}
-        shouldPause={false}
-        enableOnTouchDevice={true}
-      >
-        <img
-          className="absolute h-full w-full scale-[103%] object-cover object-center"
-          src={z3}
-        />
-      </MouseParallax>
+        <MouseParallax
+          isAbsolutelyPositioned
+          strength={0.03}
+          zIndex={3}
+          shouldPause={false}
+          enableOnTouchDevice={true}
+        >
+          <img
+            className="absolute h-full w-full scale-[103%] object-cover object-center"
+            src={z3}
+          />
+        </MouseParallax>
 
-      <MouseParallax
-        isAbsolutelyPositioned
-        strength={0.04}
-        zIndex={4}
-        shouldPause={false}
-        enableOnTouchDevice={true}
-      >
-        <img
-          className="absolute h-full w-full scale-[104%] object-cover object-center"
-          src={z4}
-        />
-      </MouseParallax>
+        <MouseParallax
+          isAbsolutelyPositioned
+          strength={0.05}
+          zIndex={5}
+          shouldPause={false}
+          enableOnTouchDevice={true}
+        >
+          <img
+            className="absolute h-full w-full scale-[105%] object-cover object-center"
+            src={z5}
+          />
+        </MouseParallax>
 
-      <MouseParallax
-        isAbsolutelyPositioned
-        strength={0.05}
-        zIndex={5}
-        shouldPause={false}
-        enableOnTouchDevice={true}
-      >
-        <img
-          className="absolute h-full w-full scale-[105%] object-cover object-center"
-          src={z5}
-        />
-      </MouseParallax>
+        <MouseParallax
+          isAbsolutelyPositioned
+          strength={0.1}
+          zIndex={6}
+          shouldPause={false}
+          enableOnTouchDevice={true}
+        >
+          <img
+            className="absolute h-full w-full scale-[110%] object-cover object-center"
+            src={z6}
+          />
+        </MouseParallax>
 
-      <MouseParallax
-        isAbsolutelyPositioned
-        strength={0.1}
-        zIndex={6}
-        shouldPause={false}
-        enableOnTouchDevice={true}
-      >
-        <img
-          className="absolute h-full w-full scale-[110%] object-cover object-center"
-          src={z6}
-        />
-      </MouseParallax>
+        <MouseParallax
+          isAbsolutelyPositioned
+          strength={0.1}
+          zIndex={7}
+          shouldPause={false}
+          enableOnTouchDevice={true}
+        >
+          <img
+            className="absolute h-full w-full scale-[110%] object-cover object-center"
+            src={z7}
+          />
+        </MouseParallax>
 
-      <MouseParallax
-        isAbsolutelyPositioned
-        strength={0.1}
-        zIndex={7}
-        shouldPause={false}
-        enableOnTouchDevice={true}
-      >
-        <img
-          className="absolute h-full w-full scale-[110%] object-cover object-center"
-          src={z7}
-        />
-      </MouseParallax>
+        <MouseParallax
+          isAbsolutelyPositioned
+          strength={0.15}
+          zIndex={8}
+          shouldPause={false}
+          enableOnTouchDevice={true}
+        >
+          <img
+            className="absolute h-full w-full scale-[115%] object-cover object-center"
+            src={z8}
+          />
+        </MouseParallax>
 
-      <MouseParallax
-        isAbsolutelyPositioned
-        strength={0.15}
-        zIndex={8}
-        shouldPause={false}
-        enableOnTouchDevice={true}
-      >
-        <img
-          className="absolute h-full w-full scale-[115%] object-cover object-center"
-          src={z8}
-        />
-      </MouseParallax>
-
-      <MouseParallax
-        isAbsolutelyPositioned
-        strength={0.25}
-        zIndex={9}
-        shouldPause={false}
-        enableOnTouchDevice={true}
-      >
-        <img
-          className="pointer-events-none absolute h-full w-full object-cover object-center"
-          src={z9}
-        />
-      </MouseParallax>
+        <MouseParallax
+          isAbsolutelyPositioned
+          strength={0.25}
+          zIndex={9}
+          shouldPause={false}
+          enableOnTouchDevice={true}
+        >
+          <img
+            className="pointer-events-none absolute h-full w-full object-cover object-center"
+            src={z9}
+          />
+        </MouseParallax>
       </div>
     </>
   );
--- src/components/HomeParaGame.jsx
+++ src/components/HomeParaGame.jsx
@@ -6,110 +6,110 @@ import z2 from "../assets/game/home-game-z50.webp";
 import z3 from "../assets/game/home-game-z60.webp";
 import z4 from "../assets/game/home-game-z70.webp";
 import z5 from "../assets/game/home-game-z80.webp";
-import z6 from "../assets/game/home-game-z90.gif";
+import z6 from "../assets/game/home-game-z90.png";
 import z7 from "../assets/game/home-game-z100.webp";
 
 export default function HomeParaGame() {
   return (
     <>
-    <div className="relative h-full overflow-hidden [contain:layout_paint]">
-      <Icon
-        icon="streamline-logos:youtube-clip-logo-solid"
-        className="absolute bottom-10 left-10 z-20 h-6 w-6 cursor-pointer text-white"
-        onClick={() =>
-          window.open("https://youtu.be/YxCVZVR6xT4", "_blank").focus()
-        }
-      />
+      <div className="relative h-full overflow-hidden [contain:layout_paint]">
+        <Icon
+          icon="streamline-logos:youtube-clip-logo-solid"
+          className="absolute bottom-10 left-10 z-20 h-6 w-6 cursor-pointer text-white"
+          onClick={() =>
+            window.open("https://youtu.be/YxCVZVR6xT4", "_blank").focus()
+          }
+        />
 
-      <img
-        className="absolute h-full w-full object-cover object-center"
-        src={z1}
-        style={{ imageRendering: "pixelated" }}
-      />
-
-      <MouseParallax
-        isAbsolutelyPositioned
-        strength={0.05}
-        zIndex={5}
-        shouldPause={false}
-        enableOnTouchDevice={true}
-      >
         <img
-          className="absolute h-full w-full scale-[105%] object-cover object-center"
-          src={z2}
+          className="absolute h-full w-full object-cover object-center"
+          src={z1}
           style={{ imageRendering: "pixelated" }}
         />
-      </MouseParallax>
 
-      <MouseParallax
-        isAbsolutelyPositioned
-        strength={0.1}
-        zIndex={6}
-        shouldPause={false}
-        enableOnTouchDevice={true}
-      >
-        <img
-          className="absolute h-full w-full scale-[110%] object-cover object-center"
-          src={z3}
-          style={{ imageRendering: "pixelated" }}
-        />
-      </MouseParallax>
+        <MouseParallax
+          isAbsolutelyPositioned
+          strength={0.05}
+          zIndex={5}
+          shouldPause={false}
+          enableOnTouchDevice={true}
+        >
+          <img
+            className="absolute h-full w-full scale-[105%] object-cover object-center"
+            src={z2}
+            style={{ imageRendering: "pixelated" }}
+          />
+        </MouseParallax>
 
-      <MouseParallax
-        isAbsolutelyPositioned
-        strength={0.2}
-        zIndex={7}
-        shouldPause={false}
-        enableOnTouchDevice={true}
-      >
-        <img
-          className="absolute h-full w-full scale-[120%] object-cover object-center"
-          src={z4}
-          style={{ imageRendering: "pixelated" }}
-        />
-      </MouseParallax>
+        <MouseParallax
+          isAbsolutelyPositioned
+          strength={0.1}
+          zIndex={6}
+          shouldPause={false}
+          enableOnTouchDevice={true}
+        >
+          <img
+            className="absolute h-full w-full scale-[110%] object-cover object-center"
+            src={z3}
+            style={{ imageRendering: "pixelated" }}
+          />
+        </MouseParallax>
 
-      <MouseParallax
-        isAbsolutelyPositioned
-        strength={0.22}
-        zIndex={8}
-        shouldPause={false}
-        enableOnTouchDevice={true}
-      >
-        <img
-          className="absolute h-full w-full scale-[122%] object-cover object-center"
-          src={z5}
-          style={{ imageRendering: "pixelated" }}
-        />
-      </MouseParallax>
+        <MouseParallax
+          isAbsolutelyPositioned
+          strength={0.2}
+          zIndex={7}
+          shouldPause={false}
+          enableOnTouchDevice={true}
+        >
+          <img
+            className="absolute h-full w-full scale-[120%] object-cover object-center"
+            src={z4}
+            style={{ imageRendering: "pixelated" }}
+          />
+        </MouseParallax>
 
-      <MouseParallax
-        isAbsolutelyPositioned
-        strength={0.25}
-        zIndex={9}
-        shouldPause={false}
-        enableOnTouchDevice={true}
-      >
-        <img
-          className="absolute h-full w-full scale-[125%] object-cover object-center"
-          src={z6}
-          style={{ imageRendering: "pixelated" }}
-        />
-      </MouseParallax>
+        <MouseParallax
+          isAbsolutelyPositioned
+          strength={0.22}
+          zIndex={8}
+          shouldPause={false}
+          enableOnTouchDevice={true}
+        >
+          <img
+            className="absolute h-full w-full scale-[122%] object-cover object-center"
+            src={z5}
+            style={{ imageRendering: "pixelated" }}
+          />
+        </MouseParallax>
 
-      <MouseParallax
-        isAbsolutelyPositioned
-        strength={0.25}
-        zIndex={10}
-        shouldPause={false}
-        enableOnTouchDevice={true}
-      >
-        <img
-          className="pointer-events-none absolute h-full w-full scale-[125%] object-cover object-center"
-          src={z7}
-          style={{ imageRendering: "pixelated" }}
-        />
-      </MouseParallax>
+        <MouseParallax
+          isAbsolutelyPositioned
+          strength={0.25}
+          zIndex={9}
+          shouldPause={false}
+          enableOnTouchDevice={true}
+        >
+          <img
+            className="absolute h-full w-full scale-[125%] object-cover object-center"
+            src={z6}
+            style={{ imageRendering: "pixelated" }}
+          />
+        </MouseParallax>
+
+        <MouseParallax
+          isAbsolutelyPositioned
+          strength={0.25}
+          zIndex={10}
+          shouldPause={false}
+          enableOnTouchDevice={true}
+        >
+          <img
+            className="pointer-events-none absolute h-full w-full scale-[125%] object-cover object-center"
+            src={z7}
+            style={{ imageRendering: "pixelated" }}
+          />
+        </MouseParallax>
       </div>
     </>
   );
